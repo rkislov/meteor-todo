ca3f0cb790a84d1244aa6b8e963f529a86b216f7
roman@kislovs.ru <Rkislov2006>
2018-01-22 13:42:21 +0500

    second commit, add mongodb and momentjs for date formatting

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -1,11 +1,30 @@
 import { Template } from 'meteor/templating';
+import { Tasks } from '../api/tasks.js';
+
 
 import './body.html';
 
 Template.body.helpers({
-    tasks: [
-        {text: 'this is task 1'},
-        {text: 'this is task 2'},
-        {text: 'this is task 3'},
-    ]
+    tasks(){
+        return Tasks.find({}, {sort:{createdAt:-1}});
+    },
+});
+Template.registerHelper('formatDate', function(date) {
+    return moment(date).format('DD-MM-YYYY HH:mm');
+  });
+Template.body.events({
+    'submit .new-task'(event){
+        event.preventDefault();
+
+        const target = event.target;
+        const text = target.text.value;
+
+
+        Tasks.insert({
+            text,
+            createdAt: new Date(),
+        })
+
+        text.target.value = '';
+    },
 });
