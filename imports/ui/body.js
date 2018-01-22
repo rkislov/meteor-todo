dec129ee4de643070d9ec5a99bb55eb97d6e97ca
roman@kislovs.ru <Rkislov2006>
2018-01-22 14:23:49 +0500

    add toggle for completed task and count of incompleted

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -1,13 +1,26 @@
 import { Template } from 'meteor/templating';
+import { ReactiveDict } from 'meteor/reactive-dict';
+
 import { Tasks } from '../api/tasks.js';
 
 import './tasks.js';
 import './body.html';
 
+Template.body.onCreated(function bodyOnCreated(){
+   this.state = new ReactiveDict(); 
+});
+
 Template.body.helpers({
     tasks(){
+        const instance = Template.instance();
+        if (instance.state.get('hideCompleted')) {
+            return Tasks.find({checked: {$ne: true}}, {sort: {createdAt: -1}});
+        }
         return Tasks.find({}, {sort:{createdAt: -1}});
     },
+    incompletedCount() {
+        return Tasks.find({ checked: {$ne: true }} ).count();
+    }
 });
 Template.registerHelper('formatDate', function(date) {
     return moment(date).format('DD-MM-YYYY HH:mm');
@@ -27,4 +40,7 @@ Template.body.events({
 
         target.text.value = '';
     },
+    'change .hide-completed input'(event, instance) {
+        instance.state.set('hideCompleted', event.target.checked);
+    }
 });
